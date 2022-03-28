5814380a59d7088d87c1e24dad569ba537b27d56
Kristof <kristof.horvath@exagent.group>
2022-03-28 09:52:08 +0200

    added views

---
+++ app/src/main/java/com/example/tvshowapp/ui/DetailView.kt
@@ -0,0 +1,11 @@
+package com.example.tvshowapp.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+
+class DetailView: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/tvshowapp/ui/SearchView.kt
@@ -0,0 +1,10 @@
+package com.example.tvshowapp.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class SearchView:ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
