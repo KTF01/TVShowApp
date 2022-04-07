c08759a558908af99d701ade9bc43fc719672b83
Kristof <kristof.horvath@exagent.group>
2022-04-07 16:06:25 +0200

    skeleton classes added

--- app/src/main/java/com/example/tvshowapp/model/TVShow.kt
+++ app/src/main/java/com/example/tvshowapp/model/TVShow.kt
@@ -1,3 +1,4 @@
 package com.example.tvshowapp.model
 
-data class TVShow(val title:String)
+data class TVShow(val title:String, val language:String, val status:String, val runtime:Int, val premiered: String) {
+}
---
+++ app/src/main/java/com/example/tvshowapp/repository/TVShowRepository.kt
@@ -0,0 +1,4 @@
+package com.example.tvshowapp.repository
+
+class TVShowRepository {
+}
